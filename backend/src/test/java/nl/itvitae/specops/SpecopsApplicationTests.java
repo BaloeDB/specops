988af1d9d97d086bf9f0e45cd313eace94db1512
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-06-10 20:22:47 +0200

    Removing print envs test

--- backend/src/test/java/nl/itvitae/specops/SpecopsApplicationTests.java
+++ backend/src/test/java/nl/itvitae/specops/SpecopsApplicationTests.java
@@ -5,15 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpecopsApplicationTests {
-  @Test
-  void printEnvs() {
-    System.out.println(System.getenv("POSTGRES_DB"));
-    System.out.println(System.getenv("POSTGRES_USERNAME"));
-    System.out.println(System.getenv("POSTGRES_PASSWORD"));
-    System.out.println(System.getenv("JWT_SECRET"));
-    System.out.println(System.getenv("JWT_EXPIRATION"));
-  }
-
 	@Test
 	void contextLoads() {
 	}
