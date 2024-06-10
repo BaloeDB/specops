a2c39b0c0e983c60429dbb5f42a3dd88b8ffd22a
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-06-10 20:06:57 +0200

    No Logger

--- backend/src/test/java/nl/itvitae/specops/SpecopsApplicationTests.java
+++ backend/src/test/java/nl/itvitae/specops/SpecopsApplicationTests.java
@@ -5,15 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpecopsApplicationTests {
-  private final Logger logger = LoggerFactory.getLogger(getClass());
-  
   @Test
   void printEnvs() {
-    logger.info("POSTGRES_DB: {}", System.getenv("POSTGRES_DB"));
-    logger.info("POSTGRES_USERNAME: {}", System.getenv("POSTGRES_USERNAME"));
-    logger.info("POSTGRES_PASSWORD: {}", System.getenv("POSTGRES_PASSWORD"));
-    logger.info("JWT_SECRET: {}", System.getenv("JWT_SECRET"));
-    logger.info("JWT_EXPIRATION: {}", System.getenv("JWT_EXPIRATION"));
+    System.out.println(System.getenv("POSTGRES_DB"));
+    System.out.println(System.getenv("POSTGRES_USERNAME"));
+    System.out.println(System.getenv("POSTGRES_PASSWORD"));
+    System.out.println(System.getenv("JWT_SECRET"));
+    System.out.println(System.getenv("JWT_EXPIRATION"));
   }
 
 	@Test
